c1643bc53613b5aab7008d8e5af7dba015c305dc
TeoNemsadze <teona.nemsadze.2@iliauni.edu.ge>
2023-10-12 11:45:29 +0400

    Initial Commit

---
+++ airbnb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  airbnb
+//
+//  Created by teona nemsadze on 12.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ airbnb/airbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbApp.swift
+//  airbnb
+//
+//  Created by teona nemsadze on 12.10.23.
+//
+
+import SwiftUI
+
+@main
+struct airbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
